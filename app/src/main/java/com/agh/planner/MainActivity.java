e83cf5be2a16ad7ebfd4c682a679327407e9265b
Maciej <macwadowskii@gmail.com>
2020-05-30 21:31:08 +0200

    Basic Navigation Drawer is working

--- app/src/main/java/com/agh/planner/MainActivity.java
+++ app/src/main/java/com/agh/planner/MainActivity.java
@@ -1,13 +1,76 @@
 package com.agh.planner;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+
+
+        // Navigation bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // "hamburger icon toggle fro navbar
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_bar_open,
+                R.string.navigation_bar_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        //setting up todo-list fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new TodoListFragment()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    /**
+     * Change Fragment View when
+     * item menu in navigation bar
+     * is clicked
+     * @param menuItem
+     * @return
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_list:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TodoListFragment()).commit();
+                break;
+            case R.id.nav_new_task:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TodoNewFormFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
